8d8bd8d9c8d71c91373a764e11acd08433b5bfbc
Julia Ouimet <julia.ouimet@gmail.com>
2017-02-06 19:44:38 -0500

    app.js

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// this is the response with "Star Wars" as the search term: 
-// https://www.googleapis.com/youtube/v3/search/?part=snippet&key=AIzaSyCPWjySIA5H7YD_VOrL04seDaDdSuUS5Vw&q=Star%20Wars
 
 var YOUTUBE_BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
 
@@ -15,48 +13,14 @@ function getDataFromApi(searchTerm, callback) {
 }
 
 function displayYouTubeSearchData(data) {
-  console.log(data.items);
   console.log(data);
   var resultElement = '';
-
-  if (data.items) {
-    for (var i=0; i < data.items.length; i++) {
-      data.items.forEach(function(item) {
-      console.log(data.items[i].snippet.thumbnails.medium.url);
-      });
-    }   
-  }
-  else {
-    resultElement += '<p> No results </p>';
+  for (var i=0; i < data.items.length; i++) {
+    var currentImgURL = data.items[i].snippet.thumbnails.medium.url;
+    var videoLink = 'https://youtube.com/watch?v=' + data.items[i].id.videoId;
+    resultElement += '<a href="' + videoLink + '">' + '<img class="js-thumbnail" src="' + currentImgURL + '"/></a>';
   }
   $('.js-search-results').html(resultElement);
-
-//   function displayOMDBSearchData(data) {
-//   var resultElement = '';
-//   if (data.Search) {
-//     data.Search.forEach(function(item) {
-//      resultElement += '<p>' + item.Title + '</p>';
-//     });
-//   }
-//   else {
-//     resultElement += '<p>No results</p>';
-//   }
-  
-//   $('.js-search-results').html(resultElement);
-// }
-
-  // data.items[i].snippet.thumbnails.medium.url
-  // var resultElement = '';
-  // if (data.Search) {
-  //   data.Search.forEach(function(item) {
-  //    resultElement += "<p>" + item + "</p>";
-  //   });
-  // }
-  // else {
-  //   resultElement += '<p>No results</p>';
-  // }
-  
-  // $('.js-search-results').html(resultElement);
 }
 
 function watchSubmit() {
@@ -68,4 +32,3 @@ function watchSubmit() {
 }
 
 $(function(){watchSubmit();});
-
